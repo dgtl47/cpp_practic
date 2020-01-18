60e70625f737413b44850890f9a9aa94e4f48fb9
dgtl47 <55292955+dgtl47@users.noreply.github.com>
2020-01-18 10:30:17 +0300

    Add files via upload

---
+++ task10/task10.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <clocale>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int x, y;
+	int result = 1;
+	
+	std::cout << "Введите число: \n";
+	std::cin >> x;
+
+	std::cout << "Введите степень: \n";
+	std::cin >> y;
+	
+	for (int i = 0; i < y; ++i) 
+			result *= x;
+
+	std::cout << result << std::endl;
+return 0;
+}
