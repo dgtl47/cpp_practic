c92a9e8833b5d7e72fd81f7c123b2c88cfc942f4
dgtl47 <55292955+dgtl47@users.noreply.github.com>
2020-01-18 10:39:23 +0300

    Add files via upload

---
+++ task12/task12.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+
+	int number;
+
+	std::cout << "Введите целое положительное число для проверки:\n";
+	std::cin >> number;
+
+	for (int i = number - 1; i > 1; i--) {
+		if (number % i == 0) {
+			std::cout << "Число составное\n";
+			return 0;
+		}
+	}
+
+	std::cout << "Число простое\n";
+
+	return 0;
+}
