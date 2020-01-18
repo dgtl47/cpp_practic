d7e055fd6d322e1121ffaec1149b0a0cf32d8941
dgtl47 <55292955+dgtl47@users.noreply.github.com>
2020-01-18 10:24:56 +0300

    Add files via upload

---
+++ task8/task8.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	double a, b;
+	char switch_on(',');
+
+	std::cout << "Введите любое вещественное число: \n";
+	std::cin >> a;
+	
+	std::cout << "Введите знак операции: \n";
+	std::cin >> switch_on;
+	
+	std::cout << "Введите любое вещественное число: \n";
+	std::cin >> b;
+	
+	switch (switch_on)
+	{
+	case '+':
+		std::cout << a << " + " << b << " = " << a + b << std::endl;
+		break;
+	
+	case '-':
+		std::cout << a << " - " << b << " = " << a - b << std::endl;
+		break;
+	
+	case '*':
+		std::cout << a << " * " << b << " = " << a * b << std::endl;
+		break;
+	
+	case '/':
+		if (b == 0)
+			std::cout << "Ошибка! На ноль делить нельзя! \n";
+		else std::cout << a << " / " << b << " = " << a / b << std::endl;
+		break;
+
+	default:
+		std::cout << "Ошибка! Введите корректное значение: \n";
+		break;
+	}
+
+	return 0;
+}
