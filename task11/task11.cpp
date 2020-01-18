d02116826015be2fceae1216017003ecc0d91a71
dgtl47 <55292955+dgtl47@users.noreply.github.com>
2020-01-18 10:36:30 +0300

    Add files via upload

---
+++ task11/task11.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <clocale>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int x;
+	int result = 1;
+
+	std::cout << "Введите целое положительное число: \n";
+	std::cin >> x;
+
+	for (int i = 1; i < x+1; ++i)
+		result *= i;
+
+	std::cout << "Факториал числа равен: \n" << result << std::endl;
+	return 0;
+}
